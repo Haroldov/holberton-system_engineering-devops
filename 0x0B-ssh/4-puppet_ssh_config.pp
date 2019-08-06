cedeff4b412575806177dc3c414f4c70b593f1f3
Haroldo Vélez <hvelez020@gmail.com>
2019-08-06 20:31:05 +0000

    [add] task #4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,18 +1,7 @@
 # Requirements:
 # Your SSH client configuration must be configured to use the private key ~/.ssh/holberton
 # Your SSH client configuration must be configured to refuse to authenticate using a password
-
-file { '/etc/ssh/ssh_config':
-  ensure => present,
-}->
-file_line { 'Append a line to /etc/ssh/ssh_config':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/holberton',
-}
-file { '/etc/ssh/ssh_config':
-  ensure => present,
-}->
-file_line { 'Replace a line to /etc/ssh/ssh_config':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
+exec { '/etc/ssh/ssh_config':
+  command => '/bin/echo -e "IdentityFile ~/.ssh/holberton\nPasswordAuthentication no" >> /etc/ssh/ssh_config',
+  path    => '/etc/ssh/ssh_config',
 }
