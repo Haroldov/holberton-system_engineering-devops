199665b5334385b95862f31980913eb3e50d8cef
Haroldo <hvelez020@gmail.com>
2019-09-05 17:11:49 +0000

    [update] db

--- 0x14-mysql/config_holSQL.sql
+++ 0x14-mysql/config_holSQL.sql
@@ -2,8 +2,9 @@
 -- Create a MySQL user named holberton_user on both web-01 and web-02 with
 -- the host name set to localhost and the password projectcorrection280hbtn.
 -- This will allow us to access the replication status on both servers.
-CREATE DATABASE IF NOT EXISTS tyrell_corp;
 GRANT ALL ON *.* TO 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+CREATE DATABASE IF NOT EXISTS tyrell_corp;
+USE tyrell_corp;
 CREATE TABLE IF NOT EXISTS nexus6 (id INT,
        	name VARCHAR(256),
 	PRIMARY KEY (id));
