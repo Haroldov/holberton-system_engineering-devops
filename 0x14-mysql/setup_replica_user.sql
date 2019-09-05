cf30775f784ac953400b6d43aba7f798e6897571
root <hvelez020@gmail.com>
2019-09-05 17:52:47 +0000

    [fix] not SAVE but SLAVE

--- 0x14-mysql/setup_replica_user.sql
+++ 0x14-mysql/setup_replica_user.sql
@@ -1,4 +1,3 @@
 -- Script that prepares a MySQL server for the project
-GRANT REPLICATION SAVE ON *.* TO 'replica_user'@'%' IDENTIFIED BY 'replicahbtn';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%' IDENTIFIED BY 'replicahbtn';
 FLUSH PRIVILEGES;
-
