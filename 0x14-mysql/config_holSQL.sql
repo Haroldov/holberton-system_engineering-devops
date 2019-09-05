1acf26cd8603ad07dd8f17144f63171b67fed0d3
root <hvelez020@gmail.com>
2019-09-05 16:31:07 +0000

    [add] mysql script to configure db

---
+++ 0x14-mysql/config_holSQL.sql
@@ -0,0 +1,5 @@
+-- Script that prepares a MySQL server for the project
+-- Create a MySQL user named holberton_user on both web-01 and web-02 with
+-- the host name set to localhost and the password projectcorrection280hbtn.
+-- This will allow us to access the replication status on both servers.
+GRANT ALL ON *.* TO 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
