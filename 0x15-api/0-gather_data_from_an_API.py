0353b431dd2bbd42567356e09c84a18cfed4face
Haroldo Vélez <hvelez020@gmail.com>
2019-09-17 02:05:06 +0000

    [fix] when input is not an int

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,6 +6,10 @@ from sys import argv
 
 if __name__ == "__main__" and len(argv) == 2:
     url = "https://jsonplaceholder.typicode.com/todos"
+    try:
+        int(argv[1])
+    except ValueError as e:
+        exit
     data = {"userId": argv[1]}
     resp = requests.get(url, params=data).json()
     url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
