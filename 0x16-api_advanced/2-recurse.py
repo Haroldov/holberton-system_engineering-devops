f72a248fd5ee57bcb26333b23f48bcbdb5de7c5f
Haroldo Vélez <hvelez020@gmail.com>
2019-09-19 20:31:35 +0000

    [fix] infinite loop

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,21 +4,19 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """ doc """
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    query = {'limit': 100}
+    query = {'limit': 100, 'after': after}
     resp = requests.get(url, headers={'User-Agent': 'Haroldo'},
                         allow_redirects=False, params=query)
-    if resp.status_code == 200:
-        objs = resp.json().get('data').get('children')
-        for obj in objs:
-            hot_list.append(obj.get('data').get('title'))
-        nxt = resp.json().get('data').get('after')
-        if nxt is not None:
-            return recurse(subreddit, hot_list)
-        else:
-            return hot_list
-    else:
+    if resp.status_code != 200:
         return None
+    nxt = resp.json().get('data').get('after')
+    if nxt is not None:
+        recurse(subreddit, hot_list, nxt)
+    objs = resp.json().get('data').get('children')
+    for obj in objs:
+        hot_list.append(obj.get('data').get('title'))
+    return hot_list
